eaef4684dd7fc2a9cd1021a69c917bcaafe398c5
muratokur3 <muratokur3@icloud.com>
2023-08-03 18:20:32 +0300

    master

--- colorGame.js
+++ colorGame.js
@@ -11,15 +11,19 @@ colors = [
   "cadetblue",
 ];
 
-const createBoxColor = (line, column) => {
-  const getRandomArray = getRandomColor(line * column);
-  //console.log(getRandomArray);
+let eldekiler = [];
+
+const createBoxColor = (sayi) => {
+  const getRandomArray = getRandomColor(sayi);
+
   let sayac = 0;
-  for (let i = 0; i < line * column; i++) {
-    let createDiv = document.createElement("div");
+  for (let i = 0; i < sayi; i++) {
+    let createDiv = document.createElement("button");
     createDiv.classList = "cardBox";
-    createDiv.style.backgroundColor = colors[getRandomArray[sayac]];
-    createDiv.setAttribute("id", sayac);
+   createDiv.style.backgroundColor = "chocolate";
+    createDiv.setAttribute("id", sayac + 1);
+    createDiv.setAttribute("value", getRandomArray[sayac]);
+    createDiv.setAttribute("onclick", "kontrolEt(this)");
     document.getElementById("gameCard").appendChild(createDiv);
     sayac++;
   }
@@ -37,4 +41,29 @@ const getRandomColor = (cardNumber) => {
   return randomNumber;
 };
 
-createBoxColor(5, 4);
+const kontrolEt = (kutucuk) => {
+
+  if (eldekiler.length == 0) {
+    eldekiler[0] = kutucuk.id;
+    kutucuk.style.backgroundColor = colors[kutucuk.value];
+  } else if (eldekiler.length == 1) {
+    eldekiler[1] = kutucuk.id;
+    kutucuk.style.backgroundColor = colors[kutucuk.value];
+
+    setTimeout(function () {
+      if (
+        document.getElementById(eldekiler[0]).style.backgroundColor ==
+        document.getElementById(eldekiler[1]).style.backgroundColor
+      ) {
+        eldekiler = [];
+      } else {
+        kutucuk.style.backgroundColor = "chocolate";
+        document.getElementById(eldekiler[0]).style.backgroundColor = "chocolate";
+        eldekiler = [];
+      }
+    }, 1000);
+    
+  }
+};
+
+createBoxColor(20);
